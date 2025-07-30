d543b21ced239ba7da2d35189803a1e7c366cdfd
Sohrab Kolsoumi <kolsoomi57@yahoo.com>
2025-07-29 21:14:04 -0500

    Add files via upload

---
+++ agrimetsoft/settings.py
@@ -0,0 +1,20 @@
+
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.environ.get('SECRET_KEY', 'insecure-secret')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = ['django.contrib.staticfiles', 'main']
+MIDDLEWARE = ['django.middleware.security.SecurityMiddleware']
+ROOT_URLCONF = 'agrimetsoft.urls'
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [BASE_DIR / 'templates'],
+    'APP_DIRS': True,
+    'OPTIONS': {'context_processors': ['django.template.context_processors.request']},
+}]
+WSGI_APPLICATION = 'agrimetsoft.wsgi.application'
+STATIC_URL = '/static/'
--- agrimetsoft/wsgi.py
+++ agrimetsoft/wsgi.py
@@ -1,1 +1,5 @@
 
+import os
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agrimetsoft.settings')
+application = get_wsgi_application()
